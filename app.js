91a6dce8953f67960cfed8603f908070de9912fd
pabitzkyt-sys <pabitzkyt@gmail.com>
2026-06-27 16:01:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 let data = {
   mechanics: ["John Smith", "Dave Brown", "Alex Carter"],
-  helpers: ["Mike Jones", "Chris Lee", "Sam White"]
+  helpers: ["Mike Jones", "Chris Lee", "Sam White"],
+  log: []
 };
 
 // ---------- LOAD ----------
+function load() {
+  let saved = localStorage.getItem("otDispatch");
+  if (saved) data = JSON.parse(saved);
+}
+
+// ---------- SAVE ----------
+function save() {
+  localStorage.setItem("otDispatch", JSON.stringify(data));
+}
+
+// ---------- INIT ----------
 function init() {
+  load();
   updateUI();
 }
 
@@ -19,15 +32,28 @@ function rotate(list) {
   list.push(list.shift());
 }
 
+// ---------- LOG ----------
+function addLog(type, name, action) {
+  data.log.unshift({
+    type,
+    name,
+    action,
+    time: new Date().toLocaleTimeString()
+  });
+
+  if (data.log.length > 200) data.log.pop();
+}
+
 // ---------- ACTION ----------
 function vote(type, action) {
 
-  if (type === "mechanic") {
-    rotate(data.mechanics);
-  } else {
-    rotate(data.helpers);
-  }
+  let list = type === "mechanic" ? data.mechanics : data.helpers;
+  let name = list[0];
 
+  addLog(type, name, action);
+
+  rotate(list);
+  save();
   updateUI();
 }
 
